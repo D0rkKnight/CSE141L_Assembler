ffb00f41bd958cacc24c23b1d1d80754a38ee2cb
Hanzen <shouhanzen@gmail.com>
2023-10-30 15:23:28 -0700

    More tooling changes for building assemblies

--- assembler/assembler.py
+++ assembler/assembler.py
@@ -5,8 +5,9 @@ opcodes = {
     "lw": "000",
     "sw": "001",
     "and": "010",
-    "inc": "011",
-    "dec": "100",
+    "imm": "011", # Load immediate
+    # "inc": "011",
+    # "dec": "100",
     "lsl": "101",
     "xor": "110",
     "beq": "111",
@@ -30,29 +31,21 @@ def assemble_instruction(instruction):
             raise ValueError(f"Lookup index {lookup_index} is too large")
         
         return opcodes[operation] + rd + lookup_index
+    
+    if operation == "imm":
+        rd = format(int(parts[1][1]), '03b')
+        imm = format(int(parts[2]), '03b')
+        
+        if len(imm) > 3:
+            raise ValueError(f"Immediate {imm} is too large")
+        
+        return opcodes[operation] + rd + imm
 
     raise ValueError(f"Unknown instruction {operation}")
 
 def assemble_program(assembly_code):
     """Convert a program (list of assembly instructions) to machine code."""
     return [assemble_instruction(instruction) for instruction in assembly_code]
-
-# Example program
-assembly_program = [
-    "lw r0 r7",
-    "sw r0 r6",
-    "and r0 r1",
-    "inc r1 r0",
-    "dec r1 r0",
-    "lsl r1 r0",
-    "xor r0 r1",
-    "beq r0 7"
-]
-
-machine_code = assemble_program(assembly_program)
-for code in machine_code:
-    print(code)
-
 
 def main():
     if len(sys.argv) < 2:
--- tests/test_assemblies/test.s
+++
@@ -1,8 +0,0 @@
-lw r0 r7
-sw r0 r6
-and r0 r1
-inc r1 r0
-dec r1 r0
-lsl r1 r0
-xor r0 r1
-beq r0 7
